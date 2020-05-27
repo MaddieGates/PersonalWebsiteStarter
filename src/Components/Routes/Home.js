8ae559d177b60ace5111382bb6eac722427f105b
Maddie Gates <maddie.gates@houserater.com>
2020-05-27 17:25:48 -0500

    Added new route and updated the about page

--- src/Components/Routes/Home.js
+++ src/Components/Routes/Home.js
@@ -3,18 +3,42 @@ import { connect } from 'react-redux';
 import { RouteDefinitions } from "../../Constants/Routes";
 import { useTheme } from '@material-ui/core/styles';
 
-import useNavigationStyles from '../../Styles/NavigationStyles'
+
+import {
+    Typography
+}   from '@material-ui/core';
+
+import useHomeStyles from '../../Styles/HomeStyles'
 
 function Home(props) {
     const {
     } = props;
 
-    const classes = useNavigationStyles();
+    const classes = useHomeStyles();
     const theme = useTheme();
 
     return (
-        <div>
-            <label>This is {RouteDefinitions.HOME.label}. Write code here to design it!</label>
+        <div className={classes.column}>
+            <div
+                className={classes.row}
+                style={{
+                    minHeight: 500
+                }}
+            >
+                <div className={classes.column}>
+                    <Typography variant={'h2'}>
+                        Maddie Gates
+                    </Typography>
+                    <Typography  variant={'subtitle 1'}>
+                        Software Development Intern at HouseRater
+                    </Typography>
+                </div>
+                <div classname={classes.column}>
+                    <Typography variant={'h4'}>
+                        put picture here
+                    </Typography>
+                </div>
+            </div>
         </div>
     );
 }
